5641c855260f46506b173f453cfa42c3528d8335
Endrit Jakupi <ej30159@seeu.edu.mk>
2024-01-11 16:12:35 +0100

    Simplified the routes

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 
-Route::resource('home', \App\Http\Controllers\HomeController::class);
-Route::resource('product', \App\Http\Controllers\ProductController::class);
+Route::resource('home', HomeController::class);
+Route::resource('product', ProductController::class);
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
-Route::get('product', [\App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
-Route::post('product', [\App\Http\Controllers\ProductController::class, 'store'])->name('product.store');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('product', [ProductController::class, 'index'])->name('product.index');
+Route::post('product', [ProductController::class, 'store'])->name('product.store');
 
 
