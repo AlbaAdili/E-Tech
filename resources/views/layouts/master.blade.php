bd0d1f3c3a4e16d47ed8089606134178173b4e68
Endrit <ej30159@seeu.edu.mk>
2024-01-12 15:17:01 +0100

    Fixed the redirection to home page when clicking the Home link in the navigation bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,7 +38,7 @@
                 </a>
                 </div>
                 <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2">Home</a></li>
+                <li><a href="{{ route('home.index') }}" class="nav-link px-2">Home</a></li>
                 <li><a href="#" class="nav-link px-2">About</a></li>
                 <li><a href="{{ route('product.index') }}" class="nav-link px-2">Shop</a></li>
                 <li><a href="{{ route('faqs.index') }}" class="nav-link px-2">FAQs</a></li>
